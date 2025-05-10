41c4636101e950f531155f7964864868ececd9ea
LeeJBaxter <lee.j.baxter@googlemail.com>
2025-05-10 16:04:32 +0100

    Added screen scripts and stylesheets

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,8 @@
+import { TitleScreen } from "./title-screen";
+import { PlayerSetupScreen } from "./player-setup-screen";
+import { PlayerTurnScreen } from "./player-turn-screen";
+import { GameOverScreen } from "./game-over-screen";
+
 class Game {
     static async main() {
 
---
+++ scripts/player-turn-screen.js
@@ -0,0 +1,5 @@
+class PlayerTurnScreen {
+
+}
+
+export { PlayerTurnScreen };
