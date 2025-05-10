244f5d77ccca6905d027fd0214ee11e00bf688dd
LeeJBaxter <lee.j.baxter@googlemail.com>
2025-05-10 19:29:51 +0100

    Fixed

--- scripts/game-over-screen.js
+++ scripts/game-over-screen.js
@@ -1,4 +1,4 @@
-import { Helper } from "./helper";
+import { Helper } from "./helper.js";
 
 class GameOverScreen {
     static get #root() { return document.getElementById("playerSetupScreenRoot"); }
